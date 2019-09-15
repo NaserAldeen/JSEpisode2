dd769a2171de9338a4ac881ae59a13265e3f4d33
NaserAldeen <30509@aum.edu.kw>
2019-09-15 12:18:47 +0300

    First commit

--- pairs.js
+++ pairs.js
@@ -19,6 +19,33 @@
 ****************************************************************/
 function pairs(names) {
   // Your code goes here
+  if (!names) return []
+  let x = names.slice(0)
+  
+  let result = []
+  var i;
+  let extra = []
+
+  while (x.length > 0) {
+    // let random1 = Math.floor(Math.random()*x.length)
+    
+    // let var1 = x[random1]
+    // x.splice(random1, 1)
+    let var1 = x.getRandom()
+    if (x.length == 0) {
+     
+    result.push([var1])
+      break
+
+    }
+    let random2 = Math.floor(Math.random()*x.length)
+    let var2 = x.getRandom()
+    let temp = [var1, var2]
+    result.push(temp)
+    // x.splice(random2, 1)
+    console.log(temp)
+  }
+  return result
 }
 
 module.exports = pairs;
@@ -41,4 +68,4 @@ Array.prototype.getRandom = function () {
   return this.splice(Math.floor(Math.random()*this.length), 1)[0];
 }
 
-console.log(pairs(['Asis', 'Hamsa', 'Fawas', 'Mishmish', 'Hussein', 'Lailz', 'Mr Potato']));
+console.log(pairs(['Asis', 'Hamsa',  'Fawas', 'Mishmish', 'Hussein', 'Lailz', 'Mr Potato']));
