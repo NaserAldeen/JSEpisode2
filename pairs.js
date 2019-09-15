a505e282262a5c02739e2abe450f688c91a6bade
NaserAldeen <30509@aum.edu.kw>
2019-09-15 12:20:10 +0300

    second commit

--- pairs.js
+++ pairs.js
@@ -20,30 +20,21 @@
 function pairs(names) {
   // Your code goes here
   if (!names) return []
+
   let x = names.slice(0)
-  
   let result = []
-  var i;
-  let extra = []
 
   while (x.length > 0) {
-    // let random1 = Math.floor(Math.random()*x.length)
-    
-    // let var1 = x[random1]
-    // x.splice(random1, 1)
+
     let var1 = x.getRandom()
     if (x.length == 0) {
-     
-    result.push([var1])
+      result.push([var1])
       break
-
     }
     let random2 = Math.floor(Math.random()*x.length)
     let var2 = x.getRandom()
     let temp = [var1, var2]
     result.push(temp)
-    // x.splice(random2, 1)
-    console.log(temp)
   }
   return result
 }
